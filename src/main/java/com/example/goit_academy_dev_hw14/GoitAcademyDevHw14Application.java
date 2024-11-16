3e97ee5e4e465dcd6eb7839b889bb23e2c2c9289
ruslana <ruslana.prus@ukr.net>
2024-11-16 13:45:07 +0100

    init

---
+++ src/main/java/com/example/goit_academy_dev_hw14/GoitAcademyDevHw14Application.java
@@ -0,0 +1,13 @@
+package com.example.goit_academy_dev_hw14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoitAcademyDevHw14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoitAcademyDevHw14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goit_academy_dev_hw14/GoitAcademyDevHw14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goit_academy_dev_hw14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoitAcademyDevHw14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
